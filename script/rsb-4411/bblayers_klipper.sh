9c64c82dd5468ede3c862e52716103e75661bb5b
AstromanGaming <astromangamingofficial@gmail.com>
2025-12-18 11:05:31 -0500

    Add Klipper Intergration

---
+++ script/rsb-4411/bblayers_klipper.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cp ~/YoctianOS-Distro/script/rsb-4411/bblayers_klipper.conf ~/YoctianOS-Distro/src/rsb-4411/build_klipper/conf/bblayers.conf
---
+++ script/rsb-4411/local_klipper.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cp ~/YoctianOS-Distro/script/rsb-4411/local_klipper.conf ~/YoctianOS-Distro/src/rsb-4411/build_klipper/conf/local.conf
