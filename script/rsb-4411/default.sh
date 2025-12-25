3aaa10e17b7cd6e24beb1872555db60e775e1b5d
AstromanGaming <astromangamingofficial@gmail.com>
2025-12-25 16:37:44 -0500

    Add

--- script/rsb-4411/default.sh
+++ script/rsb-4411/default.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd ~/YoctianOS-Distro/src/rsb-4411 && MACHINE=imx6qrsb4411a1 UBOOT_CONFIG=1G DISTRO=fsl-imx-x11 source imx-setup-release.sh -b build_default && bash ~/YoctianOS-Distro/script/rsb-4411/local_default.sh && bash ~/YoctianOS-Distro/script/rsb-4411/bblayers_default.sh && bash ~/YoctianOS-Distro/script/rsb-4411/redirector_default.sh && exec bash
+cd ~/YoctianOS-Distro/src/rsb-4411 && MACHINE=imx6qrsb4411a1 UBOOT_CONFIG=1G DISTRO=fsl-imx-x11 source imx-setup-release.sh -b build_default && bash ~/YoctianOS-Distro/script/rsb-4411/local_default.sh && bash ~/YoctianOS-Distro/script/rsb-4411/bblayers_default.sh && bash ~/YoctianOS-Distro/script/rsb-4411/redirector_default.sh && bash cp ~/YoctianOS-Distro/script/update.sh.template ~/YoctianOS-Distro/src/rsb-4411/build_default/update.sh && exec bash
